cf313fdf07dfe38af690d44e2d95d84a606d70bd
alexeybtw <alexeybtw@gmail.com>
2018-11-03 21:17:35 +0300

    Singleton Pattern was implemented

---
+++ DesignPatterns/src/com/my/sandbox/designpatterns/patterns/Singleton.java
@@ -0,0 +1,25 @@
+package com.my.sandbox.designpatterns.patterns;
+
+public class Singleton {
+	
+	private static volatile Singleton instance;
+	private volatile String testField;
+	
+	private Singleton() {
+	}
+	
+	public static synchronized Singleton getInstance() {
+		if (instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+	
+	public synchronized String getTestField() {
+		return testField;
+	}
+	
+	public synchronized void setTestField(String testField) {
+		this.testField = testField;
+	}	
+}
--- DesignPatterns/src/com/my/sandbox/designpatterns/program/MainProgram.java
+++ DesignPatterns/src/com/my/sandbox/designpatterns/program/MainProgram.java
@@ -1,10 +1,23 @@
 package com.my.sandbox.designpatterns.program;
 
+import com.my.sandbox.designpatterns.patterns.Singleton;
+
 public class MainProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SingletonExample();
 	}
-
+	
+	public static void SingletonExample() {
+		Singleton singleton1 = Singleton.getInstance();
+		singleton1.setTestField("Hello World!");
+		System.out.println(String.format("Field value in the First Singleton: %s", singleton1.getTestField()));
+		
+		Singleton singleton2 = Singleton.getInstance();
+		System.out.println(String.format("Field value in the Second Singleton: %s", singleton2.getTestField()));
+		
+		singleton2.setTestField("The Field was changed from the Second Singleton");
+		System.out.println(String.format("Field value in the First Singleton: %s", singleton1.getTestField()));
+		System.out.println(String.format("Field value in the Second Singleton: %s", singleton2.getTestField()));
+	}
 }
